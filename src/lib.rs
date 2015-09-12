52b609044f68c7a0c37752d4dd95964a43fb13ce
Martin Wernstål <m4rw3r@gmail.com>
2015-09-12 23:52:18 +0200

    Make higher kinded types generic over the Fn* type

--- src/lib.rs
+++ src/lib.rs
@@ -11,124 +11,86 @@
 /// }
 /// ```
 pub trait HKT<Output> {
-    /// The type contained in the current instance.
     type Inner;
-    /// The type containing Output.
     type Result;
 }
 
-pub trait Functor<T>: HKT<T> {
-    fn map<F>(self, F) -> Self::Result
-      where F: Fn(Self::Inner) -> T;
+// TODO: How to enforce that F: Fn*(Self::Inner) -> T?
+//       Seems to require impl-specialization
+pub trait Functor<T, F>: HKT<T> {
+    fn map(self, F) -> Self::Result;
 }
 
-pub trait FunctorMut<T>: HKT<T> {
-    fn map<F>(self, F) -> Self::Result
-      where F: FnMut(Self::Inner) -> T;
-}
-
-pub trait FunctorOnce<T>: HKT<T> {
-    fn map<F>(self, F) -> Self::Result
-      where F: FnOnce(Self::Inner) -> T;
-}
-
-pub trait Monad<T>: HKT<T> {
-    fn bind<F>(self, F) -> Self::Result
-      where F: Fn(Self::Inner) -> Self::Result;
+// Unit needs a separate trait since Monad and Applicative require Fn generics which cannot
+// be inferred when calling unit
+pub trait Unit<T>: HKT<T> {
     fn unit(Self::Inner) -> Self
       where Self: HKT<T, Inner=T>;
 }
 
-pub trait MonadMut<T>: HKT<T> {
-    fn bind<F>(self, F) -> Self::Result
-      where F: FnMut(Self::Inner) -> Self::Result;
-    fn unit(Self::Inner) -> Self
-      where Self: HKT<T, Inner=T>;
+/// The Monad trait, specifying ``bind``.
+// TODO: How to enforce that F: Fn*(Self::Inner) -> Self::Result?
+//       Seems to require impl-specialization
+pub trait Monad<T, F>: Unit<T> {
+    fn bind(self, F) -> Self::Result;
 }
 
-pub trait MonadOnce<T>: HKT<T> {
-    fn bind<F>(self, F) -> Self::Result
-      where F: FnOnce(Self::Inner) -> Self::Result;
-    fn unit(Self::Inner) -> Self
-      where Self: HKT<T, Inner=T>;
+/*
+pub fn map<T, M: Functor<T, F>, F>(m: M, f: F) -> M::Result {
+    M::map(m, f)
 }
 
-mod impls {
-    use super::{HKT, Functor, FunctorMut, FunctorOnce, Monad, MonadMut, MonadOnce};
-
-    impl<T, M: FunctorMut<T>> Functor<T> for M {
-        fn map<F>(self, f: F) -> Self::Result
-          where F: FnMut(Self::Inner) -> T {
-            M::map(self, f)
-        }
-    }
-
-    impl<T, M: FunctorOnce<T>> FunctorMut<T> for M {
-        fn map<F>(self, f: F) -> Self::Result
-          where F: FnOnce(Self::Inner) -> T {
-            M::map(self, f)
-        }
-    }
-
-    impl<T, M: MonadMut<T>> Monad<T> for M {
-        fn bind<F>(self, f: F) -> Self::Result
-          where F: Fn(Self::Inner) -> Self::Result {
-            M::bind(self, f)
-        }
-
-        fn unit(t: Self::Inner) -> Self
-          where Self: HKT<T, Inner=T> {
-            M::unit(t)
-        }
-    }
-
-    impl<T, M: MonadOnce<T>> MonadMut<T> for M {
-        fn bind<F>(self, f: F) -> Self::Result
-          where F: FnMut(Self::Inner) -> Self::Result {
-            M::bind(self, f)
-        }
-
-        fn unit(t: Self::Inner) -> Self
-          where Self: HKT<T, Inner=T> {
-            M::unit(t)
-        }
-    }
+pub fn unit<T, M>(t: M::Inner) -> M
+  where M: Unit<T, Inner=T> {
+    Unit::unit(t)
 }
 
-#[test]
-fn test_a() {
-    #[derive(Debug)]
-    struct A<T> {
-        t: T,
-    }
+pub fn bind<M: Monad<T, F>, T, F>(m: M, f: F) -> M::Result {
+    Monad::bind(m, f)
+}
+*/
 
-    impl<T, U> HKT<U> for A<T> {
-        type Inner  = T;
-        type Result = A<U>;
-    }
+#[cfg(test)]
+mod test {
+    use ::{HKT, Functor, Unit, Monad};
 
-    impl<T, U> FunctorOnce<U> for A<T> {
-        fn map<F>(self, f: F) -> Self::Result
-          where F: FnOnce(Self::Inner) -> U {
-            A { t: f(self.t) }
-        }
-    }
-
-    impl<T, U> MonadOnce<U> for A<T> {
-        fn bind<F>(self, f: F) -> Self::Result
-          where F: FnOnce(Self::Inner) -> Self::Result {
-            f(self.t)
+    #[test]
+    fn test_simple_container() {
+        #[derive(Debug)]
+        struct A<T> {
+            t: T,
         }
 
-        fn unit(t: Self::Inner) -> Self
-          where Self: HKT<T, Inner=T> {
-            A { t: t }
+        impl<T, U> HKT<U> for A<T> {
+            type Inner  = T;
+            type Result = A<U>;
         }
+
+        impl<T, U, F> Functor<U, F> for A<T>
+          where F: FnOnce(T) -> U {
+            fn map(self, f: F) -> Self::Result {
+                A { t: f(self.t) }
+            }
+        }
+
+        impl<T, U> Unit<U> for A<T> {
+            fn unit(t: Self::Inner) -> Self
+              where Self: HKT<T, Inner=T> {
+                A { t: t }
+            }
+        }
+
+        impl<T, U, F> Monad<U, F> for A<T>
+          where F: FnOnce(T) -> A<U> {
+            fn bind(self, f: F) -> Self::Result {
+                f(self.t)
+            }
+        }
+
+        let foo = A::<i32>{ t: 2 };
+        let bar = A::<i32>{ t: 23 };
+
+        println!("{:?}", Functor::map(foo, |i| format!("{}", i)));
+        println!("{:?}", Monad::bind(bar, |i| Unit::unit(format!("{}", i))));
     }
-
-    let foo = A::<i32>{ t: 2 };
-    let bar = A::<i32>{ t: 23 };
-
-    println!("{:?}", Functor::map(foo, |i| format!("{}", i)));
-    println!("{:?}", Monad::bind(bar, |i| Monad::unit(format!("{}", i))));
 }
